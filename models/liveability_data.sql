3328df3f9a553691825d7104d94c6ad01dc46e47
Liveabilityment360@gmail.com <Liveabilityment360@gmail.com>
2022-09-07 12:20:33 +0000

    formatting changes

--- models/liveability_data.sql
+++ models/liveability_data.sql
@@ -1,108 +1,112 @@
 {{ config(materialized='table') }}
 
 WITH final_liveability AS (
-WITH choices AS (
-		SELECT SPLIT(ui2.user_interests,',') as OPTION
-					,RANK() over (
-						ORDER BY ui2.created_date DESC
-								) AS rank_no2 
-								FROM `streamdata.user_input` ui2
-)
 
-SELECT * 
-FROM
-	(
-
-	SELECT
-		 ccare.name as name   
-		,ccare.Categories as category    
-		,ccare.Address as address    
-		,ccare.City as city     
-		,ccare.Postcode as postcode    
-		,ccare.Latitude as Latitude
-		,ccare.Longitude as longitude
-	FROM transform_batchdata.childcarecentre ccare
-	UNION ALL
-	SELECT
-		 hosp.name as name   
-		,hosp.Categories as category    
-		,hosp.Address as address    
-		,hosp.City as city     
-		,hosp.Postcode as postcode    
-		,hosp.Latitude as Latitude
-		,hosp.Longitude as longitude
-	FROM transform_batchdata.hospitals hosp
-	UNION ALL 
-	SELECT
-		 rel.name as name   
-		,rel.Category as category    
-		,rel.Address as address    
-		,rel.Suburb as city     
-		,rel.Postcode as postcode    
-		,rel.Latitude as Latitude
-		,rel.Longitude as longitude
-	FROM transform_batchdata.religiousorganizations rel 
-	UNION ALL 
-	SELECT
-		 res.name as name   
-		,res.Categories as category    
-		,res.Address as address    
-		,res.City as city     
-		,res.Postcode as postcode    
-		,res.Latitude as Latitude
-		,res.Longitude as longitude
-	FROM transform_batchdata.restaurants res 
-	UNION ALL 
-	SELECT
-		 sch.name as name   
-		,sch.Categories as category    
-		,sch.Address as address    
-		,sch.City as city     
-		,sch.Postcode as postcode    
-		,sch.Latitude as Latitude
-		,sch.Longitude as longitude
-	FROM transform_batchdata.schools sch
-	UNION ALL  
-	SELECT
-		 shop.name as name
-		,shop.Categories as category
-		,shop.Address as adress
-		,shop.City as city
-		,shop.Postcode as postcode
-		,shop.Latitude as latitude
-		,shop.Longitude as longitude
-	FROM transform_batchdata.shoppingcentre shop
-	UNION ALL 
-	SELECT
-		 sport.name as name
-		,sport.Categories as category
-		,sport.Address as adress
-		,sport.City as city
-		,sport.Postcode as postcode
-		,sport.Latitude as latitude
-		,sport.Longitude as longitude
-	FROM transform_batchdata.sportsclubs sport
-
-)
-WHERE postcode = (
-    WITH LOCATION AS (
-		SELECT  ui1.confirm_new_postcode as postcode			
-				   ,RANK() over (
-						 ORDER BY ui1.created_date DESC
-      					) AS RANK_NO1
-								FROM `streamdata.user_input` ui1
-		)
-						
-		SELECT LOCATION.Postcode
-		FROM LOCATION
-		WHERE LOCATION.RANK_NO1 = 1
-	)
-
-AND category IN UNNEST((SELECT CHOICES.OPTION 
-												FROM CHOICES
-												WHERE CHOICES.RANK_NO2 = 1
-												) )
-
+  WITH choices AS (
+  	SELECT SPLIT(ui2.user_interests,',') as OPTION
+  	      ,RANK() over (
+  		   ORDER BY ui2.created_date DESC
+  		   ) AS rank_no2 
+  	FROM `streamdata.user_input` ui2
+  )
+  
+  SELECT * FROM (
+  	SELECT
+  		 ccare.name as name   
+  		,ccare.Categories as category   
+		,'Red' as cat_colour
+  		,ccare.address as address    
+  		,ccare.city as city     
+  		,ccare.postcode as postcode    
+  		,ccare.latitude as Latitude
+  		,ccare.longitude as longitude
+  	FROM transform_batchdata.childcarecentre ccare
+  	UNION ALL
+  	SELECT
+  		 hosp.name as name   
+  		,hosp.categories as category 
+		,'Blue' as cat_colour
+  		,hosp.address as address    
+  		,hosp.city as city     
+  		,hosp.postcode as postcode    
+  		,hosp.latitude as Latitude
+  		,hosp.longitude as longitude
+  	FROM transform_batchdata.hospitals hosp
+  	UNION ALL 
+  	SELECT
+  		 rel.name as name   
+  		,rel.category as category  
+		,'Green' as cat_colour
+  		,rel.address as address    
+  		,rel.suburb as city     
+  		,rel.postcode as postcode    
+  		,rel.latitude as Latitude
+  		,rel.longitude as longitude
+  	FROM transform_batchdata.religiousorganizations rel 
+  	UNION ALL 
+  	SELECT
+  		 res.name as name   
+  		,res.categories as category 
+		,'Yellow' as cat_colour
+  		,res.address as address    
+  		,res.city as city     
+  		,res.postcode as postcode    
+  		,res.latitude as Latitude
+  		,res.longitude as longitude
+  	FROM transform_batchdata.restaurants res 
+  	UNION ALL 
+  	SELECT
+  		 sch.name as name   
+  		,sch.categories as category
+		,'Orange' as cat_colour
+  		,sch.address as address    
+  		,sch.city as city     
+  		,sch.postcode as postcode    
+  		,sch.latitude as Latitude
+  		,sch.longitude as longitude
+  	FROM transform_batchdata.schools sch
+  	UNION ALL  
+  	SELECT
+  		 shop.name as name
+  		,shop.categories as category
+		,'Pink' as cat_colour
+  		,shop.address as adress
+  		,shop.city as city
+  		,shop.postcode as postcode
+  		,shop.latitude as latitude
+  		,shop.longitude as longitude
+  	FROM transform_batchdata.shoppingcentre shop
+  	UNION ALL 
+  	SELECT
+  		 sport.name as name
+  		,sport.categories as category
+		,'Purple' as cat_colour
+  		,sport.address as adress
+  		,sport.city as city
+  		,sport.postcode as postcode
+  		,sport.latitude as latitude
+  		,sport.longitude as longitude
+  	FROM transform_batchdata.sportsclubs sport
+  
+  )
+  WHERE postcode = (
+    WITH location AS (
+  	  SELECT  ui1.confirm_new_postcode as postcode			
+  			  ,RANK() OVER (
+  				 ORDER BY ui1.created_date DESC
+       		   ) AS rank_no1
+  	    FROM `streamdata.user_input` ui1
+  	)	
+  	SELECT location.Postcode
+  	  FROM location
+  	 WHERE location.rank_no1 = 1
+  	)
+  
+  AND category IN UNNEST(
+        (SELECT choices.OPTION 
+  		   FROM choices
+  		  WHERE choices.rank_no2 = 1
+  		) )
 )
 select * from final_liveability
 
